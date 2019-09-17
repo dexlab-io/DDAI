4af4e509ea04612ef511098a731b4a0d3dc90b97
Alessio Delmonti <alessiodelmonti@klm119403.cs.ad.klmcorp.net>
2019-09-17 15:24:07 +0200

    Adding navigation

--- packages/ui/src/App/index.js
+++ packages/ui/src/App/index.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import Application from 'react-rainbow-components/components/Application';
+import Routes from '../routes';
+import './styles.css';
 
 
 class App extends Component {
-
-    componentDidUpdate() {
-    }
 
     render() {
         return (
             <Application>
                 <div className="react-rainbow-admin-app_router-container">
-                    {/*<Routes />*/}
-                    Hello
+                    <Routes />
                 </div>
             </Application>
     );
---
+++ packages/ui/src/pages/dapp/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Dapp extends Component {
+
+    render() {
+        return (<span>Hello</span>);
+    }
+}
+
+export default Dapp;
